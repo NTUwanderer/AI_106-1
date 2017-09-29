9a9bdcb078f785dd38e8c340c8616a64945b2155
NTUwanderer <harveyycyang@gmail.com>
2017-09-29 16:34:21 +0800

    HW1 Q4 done

--- HW1/search.py
+++ HW1/search.py
@@ -242,7 +242,64 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    from util import PriorityQueue
+    start = problem.getStartState()
+    nodes = PriorityQueue()
+    indices = PriorityQueue()
+    steps = []
+    predecessors = []
+    costs = []
+    explored = set()
+    frontier = set()
+    heuStart = heuristic(start, problem)
+    nodes.push(start, heuStart)
+    indices.push(0, heuStart)
+    predecessors.append({'node': None, 'index': -1, 'dir': None})
+    costs.append(0)
+    frontierIndices = {}
+    frontierIndices[start] = 0
+
+    indexCounter = 1
+    while nodes.isEmpty() == False:
+        node = nodes.pop()
+        index = indices.pop()
+        cost = costs[index]
+
+        del frontierIndices[node]
+        explored.add(node)
+
+        if problem.isGoalState(node):
+            while (predecessors[index].get('node') != None):
+                predecessor = predecessors[index]
+                node = predecessor.get('node')
+                index = predecessor.get('index')
+                steps.insert(0, predecessor.get('dir'))
+            break
+
+        successors = problem.getSuccessors(node)
+        for successor in successors:
+            if successor[0] not in explored:
+                newCost = cost + successor[2]
+                heuCost = heuristic(successor[0], problem)
+                if successor[0] in frontierIndices:
+                    frontierIndex = frontierIndices[successor[0]]
+                    oldCost = costs[frontierIndex]
+                    if (newCost < oldCost):
+                        nodes.update(successor[0], newCost + heuCost)
+                        indices.udpate(frontierIndex, newCost + heuCost)
+                        predecessors[frontierIndex] = {'node': node, 'index': index, 'dir': successor[1]}
+                        costs[frontierIndex] = newCost
+                else:
+                    newIndex = indexCounter
+                    indexCounter += 1
+                    nodes.push(successor[0], newCost + heuCost)
+                    indices.push(newIndex, newCost + heuCost)
+                    frontierIndices[successor[0]] = newIndex
+                    predecessors.append({'node': node, 'index': index, 'dir': successor[1]})
+                    costs.append(newCost)
+
+    return steps
+    # util.raiseNotDefined()
 
 
 # Abbreviations
