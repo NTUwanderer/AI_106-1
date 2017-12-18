a375a7344ca2a61e0302a0ed405a171f40146571
NTUwanderer <harveyycyang@gmail.com>
2017-12-19 00:27:10 +0800

    temp6

--- kaggle_proj/temp6.py
+++ kaggle_proj/temp6.py
@@ -1,4 +1,4 @@
-from xgboost import XGBClassifier
+from xgboost import XGBClassifier, XGBRegressor
 
 import numpy as np
 import keras
@@ -7,32 +7,91 @@ from keras.layers import Dense, Activation
 from keras.utils import normalize
 from sklearn.metrics import accuracy_score
 from keras.models import load_model
+import math
 
 batch_size = 16000
 epochs = 1000
 
-data = np.genfromtxt('train.csv', delimiter=',', skip_header=1)
-np.random.seed(1)
-np.random.shuffle(data)
-data_x = data[:, 1:24]
-data_y = data[:, 24]
-data_y = data_y.astype(int)
-# read in data
-model = XGBClassifier(max_depth=10, objective='rank:pairwise')
+validate_ratio = 0.25
 
-model.fit(data_x, data_y)
-pred = model.predict(data_x)
-print ('acc: ', accuracy_score(data_y, pred))
+data = np.genfromtxt('Train.csv', delimiter=',', skip_header=1)
+data = data[:, 1:]
+np.random.seed(2)
+def getValue(x):
+    return x[-1]
+data = np.array(sorted(data, key=getValue))
+print ('shape0: ', data.shape)
+counts = int(sum(data[:, -1]))
+print ('counts: ', counts)
+
+zeros = data[:-counts]
+ones = data[-counts:]
+
+np.random.shuffle(zeros)
+np.random.shuffle(ones)
+
+vo_split = int(counts / 4)
+vz_split = 5000 - vo_split
+data = np.concatenate((zeros[:-vz_split], ones[:-vo_split]))
+validation = np.concatenate((zeros[-vz_split:], ones[-vo_split:]))
+
+print ('shapes: ', data.shape, validation.shape)
+
+np.random.shuffle(data)
+np.random.shuffle(validation)
+
+#np.random.shuffle(data)
+data_x = data[:, :-1]
+data_y = data[:, -1]
+data_y = data_y.astype(int)
+
+weight = np.array(data_y, copy=True)
+
+for i in range(len(weight)):
+    if weight[i] == 1:
+        weight[i] = math.sqrt(20000 - counts)
+    else:
+        weight[i] = math.sqrt(counts)
+
+vx = validation[:, :-1]
+vy = validation[:, -1]
+vy = vy.astype(int)
+
+
+# read in data
+model = XGBClassifier(max_depth=3, n_estimators=100, objective='rank:pairwise')
+# model = XGBRegressor(objective='rank:pairwise')
+
+def myMap500(p, y):
+    rank = np.argsort(1 - p)
+    y = y.get_label()
+    values = [0] * 500
+    for i in range(500):
+        values[i] = y[rank[i]]
+
+    total = 0.0
+    run_sum = 0.0
+
+    for i in range(500):
+        run_sum += values[i]
+        total += run_sum / (i + 1)
+
+    return 'mymap500', total / 500
+
+model.fit(data_x, data_y, eval_set=[(vx, vy)], eval_metric=myMap500, verbose=True)
+pred = model.predict_proba(vx)
+pred = pred[:, 1]
 
 data_t = np.genfromtxt('Test_Public.csv', delimiter=',', skip_header=1)
 
 prediction = model.predict_proba(data_t[:, 1:])
-prediction = normalize(prediction[:, 1])
+# prediction = model.predict(data_t[:, 1:])
+# prediction = normalize(prediction[:, 1])
 
 myPrediction = np.zeros((len(data_t)))
 
 for i in range(len(data_t)):
-    myPrediction[i] = prediction[0][i]
+    myPrediction[i] = prediction[i][1]
 
 ids = []
 
